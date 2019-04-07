98bfa70101a3eebc2633084b855ad1e0d766ec45
ziggylineous <ignacio_ferrari@hotmail.com>
2019-04-07 16:42:13 -0300

    user admin routes
    User implements pathBinder interface to map a route id to a User object
    User repository to centralize users (several classes can access users)

--- web/app/models/User.java
+++ web/app/models/User.java
@@ -1,20 +1,55 @@
 package models;
 
-public class User {
+import play.mvc.PathBindable;
+import repos.UserRepository;
+
+import java.time.LocalDateTime;
+import java.util.function.Function;
+
+public class User implements PathBindable<User> {
     private Long id;
     private String name;
     // todo: placeLists
+    private LocalDateTime lastAccess;
 
+    // este ctor está porque el pathBindable necesita una instancia
+    // para hacer el bindeo path -> objeto
     public User(Long id, String name) {
         this.id = id;
         this.name = name;
+        this.lastAccess = LocalDateTime.now();
     }
 
-    public Long getId() {
-        return id;
+    public User() {
+        id = 0L;
+        name = "";
+        lastAccess = LocalDateTime.now();
     }
 
+
+    public Long getId() { return id; }
     public String getName() {
         return name;
     }
+
+    public int listsCount() { return 1; }
+    public int placesCount(Function<Object, Boolean> predicate) {
+        // todo: filtrar lugares de listas con predicado
+        return 10;
+    }
+
+    public LocalDateTime getLastAccess() { return lastAccess; }
+
+
+    // estos metodos son para que linkee una id de la request path
+    // con un user (interface PathBindable)
+    public User bind(String key, String txt) {
+        return UserRepository.find(Long.valueOf(txt))
+            .orElseThrow(() -> new RuntimeException("Couldn't find user with id " + txt));
+    }
+
+    public String unbind(String key) {
+        return id.toString();
+    }
+    public String javascriptUnbind() { return null; }
 }
