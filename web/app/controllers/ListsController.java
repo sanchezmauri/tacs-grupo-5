e782a9f3020fff0177d1dc4a84f88ecebe8aedba
ziggylineous <ignacio_ferrari@hotmail.com>
2019-04-09 14:57:10 -0300

    list, add and remove place lists (provisionial)

--- web/app/controllers/ListsController.java
+++ web/app/controllers/ListsController.java
@@ -1,14 +1,59 @@
 package controllers;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import models.PlaceList;
 import models.Venue;
 import play.libs.Json;
 import play.mvc.Controller;
+import play.mvc.Http;
 import play.mvc.Result;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ListsController extends Controller {
+    private List<PlaceList> placeLists = new ArrayList() {{
+        add(new PlaceList(1L, "Restaurants Orientales"));
+        add(new PlaceList(2L, "Cines"));
+        add(new PlaceList(3L, "Paseo 1"));
+    }};
+
+    public Result list() {
+        return ok(Json.toJson(placeLists));
+    }
+
+    public Result create(Http.Request request) {
+        // extraer esto en algún lado tipo validar json o algo
+        JsonNode newListJson = request.body().asJson();
+
+        if (newListJson == null) {
+            return badRequest(Utils.createErrorMessage("Ill formatted json."));
+        }
+
+        if (!newListJson.has("name")) {
+            return badRequest(
+                Utils.createErrorMessage("Missing field: name")
+            );
+        }
+
+        String listName = newListJson.get("name").asText();
+        Long nextId = placeLists.get(placeLists.size() -1).getId() + 1;
+        PlaceList newList = new PlaceList(nextId, listName);
+        placeLists.add(newList);
+
+        return created(Json.toJson(newList));
+    }
+
+    public Result delete(Long listId) {
+        boolean didRemove = placeLists.removeIf(list -> list.getId().equals(listId));
+
+        if (didRemove) {
+            return ok();
+        } else {
+            return notFound(Utils.createErrorMessage("No list with id = " + listId.toString()));
+        }
+    }
+
     public Result compareLists(Long listId1, Long listId2) {
         List<Venue> commonVenues = new ArrayList<>();
         commonVenues.add(new Venue(1L, "Maxi Kiosco"));
---
+++ web/app/models/PlaceList.java
@@ -0,0 +1,28 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlaceList {
+    private Long id;
+    private String name;
+    private List<String> places;
+
+    public PlaceList(Long id, String name) {
+        this.id = id;
+        this.name = name;
+        places = new ArrayList<>();
+    }
+
+    public void addPlace(String place) {
+        places.add(place);
+    }
+
+    public void removePlace(String place) {
+        places.remove(place);
+    }
+
+    public Long getId() { return id; }
+    public String getName() { return name; }
+    public List<String> getPlaces() { return places; }
+}
