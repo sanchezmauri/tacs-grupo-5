cc8e977cd206bd64f3d3612f45493b8897620930
Martín Loguancio <martin.loguancio@gmail.com>
2019-04-24 14:38:24 -0300

    Changed implementation to webhook

--- app/tasks/TelegramTask.java
+++ app/tasks/TelegramTask.java
@@ -6,6 +6,7 @@ import services.TelegramBot;
 import play.libs.ws.WSClient;
 import scala.concurrent.ExecutionContext;
 import scala.concurrent.duration.Duration;
+import services.TelegramSetupRunnable;
 
 import javax.inject.Inject;
 import java.util.concurrent.TimeUnit;
@@ -13,23 +14,12 @@ import java.util.concurrent.TimeUnit;
 
 public class TelegramTask {
 
-    private final TelegramBot telegramBot;
-    private final ActorSystem actorSystem;
-    private final ExecutionContext executionContext;
-
     @Inject
     public TelegramTask(Config config, WSClient ws, ActorSystem actorSystem, ExecutionContext executionContext) {
-        this.telegramBot = new TelegramBot(config,ws);
-        this.actorSystem = actorSystem;
-        this.executionContext = executionContext;
 
-        this.initialize();
-    }
-
-    private void initialize() {
         actorSystem.scheduler().scheduleOnce(
                 Duration.create(0, TimeUnit.SECONDS), // initialDelay
-                telegramBot,
+                new TelegramSetupRunnable(config, ws),
                 executionContext
         );
 
