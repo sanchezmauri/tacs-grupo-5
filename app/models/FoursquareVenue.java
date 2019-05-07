a4748f17dd3d4189e048a3c491433fed794cf1e8
ziggylineous <ignacio_ferrari@hotmail.com>
2019-05-07 15:01:42 -0300

    FoursquareVenue del commit anterior

---
+++ app/models/FoursquareVenue.java
@@ -0,0 +1,36 @@
+package models;
+
+import java.time.LocalDate;
+
+public class FoursquareVenue {
+    private String id;
+    private String name;
+    private String address;
+    private LocalDate added;
+
+    public FoursquareVenue(String id, String name, String address, LocalDate added) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.added = added;
+    }
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) { this.id = id; }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) { this.name = name; }
+
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) { this.address = address; }
+
+    public LocalDate getAdded() { return added; }
+    public void setAdded(LocalDate added) { this.added = added; }
+}
+
