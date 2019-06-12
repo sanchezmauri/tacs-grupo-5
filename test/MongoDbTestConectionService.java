6c89b0665f50ca563cdd15567e4a3aabf62e4665
Martín Loguancio <martin.loguancio@gmail.com>
2019-06-12 10:54:09 -0300

    Added Test to mongo Connection
    This helped troubleshoot some issues with the mongodb driver over connection to remote.
    
    FYI - The driver version used by play is < 3.6 so the ConnectionString format must be that of the 3.4 driver.

--- test/MongoDbTestConectionService.java
+++ test/MongoDbTestConectionService.java
@@ -1,15 +1,48 @@
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
 import dev.morphia.Datastore;
 import dev.morphia.Morphia;
+import dev.morphia.mapping.MapperOptions;
+import models.Rol;
+import models.User;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class MongoDbTestConectionService {
 
+    static String clientURI = "Paste here the connection string from the TACS Gdoc";
 
-        final static Morphia morphia = new Morphia();
-        static {
+    @Test
+    public void connect(){
+        Morphia morphia = new Morphia();
+        morphia.map(User.class);
+        morphia.getMapper().setOptions(
+                MapperOptions.builder()
+                        .storeEmpties(true)
+                        .build()
+        );
+        try {
             morphia.mapPackage("models");
-            final Datastore datastore = morphia.createDatastore(new MongoClient(), "TEST");
+            Datastore datastore = morphia.createDatastore(new MongoClient(new MongoClientURI(clientURI)), "TEST");
             datastore.ensureIndexes();
+
+            User testUser = new User("Test", "John","PasswordHArd123", Rol.SYSUSER);
+            datastore.save(testUser);
+
+
+            var query = datastore.find(User.class);
+
+            Assert.assertEquals(query.count(), 1);
+
+            datastore.getDatabase().drop();
+
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+            throw e;
         }
+
+    }
+
 
 }
